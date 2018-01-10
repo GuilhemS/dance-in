7f4e2a66d5be8e86e5b3c9e0a83b925228de8cb3
Unknown <guilhem.sabathier@etu.univ-tlse2.fr>
2018-01-10 13:21:06 +0100

    pas grand chose

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
-<HTML>
-	<HEAD>
+<html>
+	<head>
 		<meta charset="UTF-8"/>
-		<meta name="title" content="Titre du site"/>
-		<meta name="description" content="Contenu"/>
+		<meta name="title" content="Dance'In"/>
+		<meta name="description" content="Site vitrine Dance'In"/>
 		<meta name="keywords" content="HTML,CSS,XML,JavaScript"/>
-		<meta name="author" content="Xavier MOULY"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>	
+		<meta name="author" content="Alice DEVESA, Guilhem SABATHIER"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<link rel="stylesheet" type="text/css" href="style.css">
-	</HEAD>
+	</head>
 
-	<BODY>
-		<HEADER>
-			<H1>Titre du site</H1>
-		</HEADER>
-		
-		<NAV>
-			<DIV>Menu 1</DIV>
-			<UL id="menu1">
-				<LI><a href="#">Lien 1</a></LI>
-				<LI><a href="#">Lien 2</LI>
-				<LI><a href="#">Lien 3</LI>
-			</UL>
-		</NAV>
-		<NAV>
+	<body>
+		<header>
+			<h1>Dance'In</h1>
+		</header>
+
+		<nav>
+			<div>Menu 1</div>
+			<ul id="menu1">
+				<li><a href="#">Lien 1</a></li>
+				<li><a href="#">Lien 2</li>
+				<li><a href="#">Lien 3</li>
+			</ul>
+		</nav>
+		<nav>
 			<DIV>Menu 2</DIV>
 			<DIV>
 				<a href="#">Lien 1</a>
@@ -31,7 +31,7 @@
 				<a href="#">Lien 3</a>
 			</DIV>
 		</NAV>
-		
+
 		<SECTION>
 
 		<h2>Titre 2</h2>
@@ -43,17 +43,14 @@
 		<p>
 		Excogitatum est super his, ut homines quidam ignoti, vilitate ipsa parum cavendi ad colligendos rumores per Antiochiae latera cuncta destinarentur relaturi quae audirent. hi peragranter et dissimulanter honoratorum circulis adsistendo pervadendoque divites domus egentium habitu quicquid noscere poterant vel audire latenter intromissi per posticas in regiam nuntiabant, id observantes conspiratione concordi, ut fingerent quaedam et cognita duplicarent in peius, laudes vero supprimerent Caesaris, quas invitis conpluribus formido malorum inpendentium exprimebat.
 		</p>
-		
+
 		<h2>Titre 2</h2>
 		<p>
 		Excogitatum est super his, ut homines quidam ignoti, vilitate ipsa parum cavendi ad colligendos rumores per Antiochiae latera cuncta destinarentur relaturi quae audirent. hi peragranter et dissimulanter honoratorum circulis adsistendo pervadendoque divites domus egentium habitu quicquid noscere poterant vel audire latenter intromissi per posticas in regiam nuntiabant, id observantes conspiratione concordi, ut fingerent quaedam et cognita duplicarent in peius, laudes vero supprimerent Caesaris, quas invitis conpluribus formido malorum inpendentium exprimebat.
 		</p>
 		</SECTION>
-		
-		<FOOTER>
-		</FOOTER>
-		<ADDRESS>
-			Xavier MOULY, <BR/>3IL Rodez, <BR/>Rue de Bruxelles 12000 BOURRAN - RODEZ
-		</ADDRESS>
-		</BODY>
-</HTML>
+
+		<footer>
+		</footer>
+	</body>
+</html>
