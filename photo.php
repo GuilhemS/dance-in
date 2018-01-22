d69a622f19e1ccadbd389b519b72ef49389c364f
Unknown <guilhem.sabathier@etu.univ-tlse2.fr>
2018-01-22 10:04:14 +0100

    Correction commit precedent et ajout photo test

--- photo.php
+++ photo.php
@@ -13,8 +13,50 @@
 	<body>
 		<?php include('include/nav.php'); ?>
 
-      <p>Bonjour le cul</p>
+      <div class="image-slider">
+         <div class="mySlide">
+            <div class="numbertext">1 / 8</div>
+            <img src="/images/slides/image1.jpg" style="width:100%">
+         </div>
 
+         <div class="mySlide">
+            <div class="numbertext">2 / 8</div>
+            <img src="/images/slides/image2.jpg" style="width:100%">
+         </div>
+
+         <div class="mySlide">
+            <div class="numbertext">3 / 8</div>
+            <img src="/images/slides/image3.jpg" style="width:100%">
+         </div>
+
+         <div class="mySlide">
+            <div class="numbertext">4 / 8</div>
+            <img src="/images/slides/image4.jpg" style="width:100%">
+         </div>
+
+         <div class="mySlide">
+            <div class="numbertext">5 / 8</div>
+            <img src="/images/slides/image5.jpg" style="width:100%">
+         </div>
+
+         <div class="mySlide">
+            <div class="numbertext">6 / 8</div>
+            <img src="/images/slides/image6.jpg" style="width:100%">
+         </div>
+
+         <div class="mySlide">
+            <div class="numbertext">7 / 8</div>
+            <img src="/images/slides/image7.jpg" style="width:100%">
+         </div>
+
+         <div class="mySlide">
+            <div class="numbertext">8 / 8</div>
+            <img src="/images/slides/image8.jpg" style="width:100%">
+         </div>
+
+         <!-- Boutons suivant et précédent -->
+         <a class="prev" onclick=""</a>
+      </div>
 		<?php include('include/footer.php'); ?>
 	</body>
 </html>
