f9c1cde866a33bee47ece76c6bb6da6225955616
Alice <alice.devesa31@gmail.com>
2018-02-05 15:02:28 +0100

    Tableau equipe geré pour reduction page

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 	<meta name="keywords" content="HTML,CSS,XML,JavaScript"/>
 	<meta name="author" content="Alice DEVESA, Guilhem SABATHIER"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="stylesheet" media="screen and (max-width: 200px) and (max-width:640p" href="responstyle.css" type="text/css"/>
 </head>
 
 <body>
 	<?php include('include/nav.php');?>
-
+<section>
 	<div class="contenu">
 
 		<?php
@@ -35,33 +36,35 @@ $reponse->closeCursor(); // Termine le traitement de la requête
 
 </div>
 
-<div class="contenu">
-	<table style="width: 745px;" align="center">
-		<tr>
-			<td>
-				<div class="circle" style="background-image:url(images/chorégraphe.png);"></div>
-			</td>
-			<td>
-				<div class="circle" style="background-image:url(images/Equipelogo.png);"></div>
-			</td>
-			<td>
-				<div class="circle" style="background-image:url(images/Equipelogo.png);"></div></div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				Marianne est la fondatrice de l'école elle enseigne la danse classique et le contemporain.
-			</td>
-			<td>
-				Jeanne enseigne depuis 10 ans maintenant le hip-hop.
-			</td>
-			<td>
-				Charlotte a fini sa formation à l'opéra de Paris la saison dernière elle enseigne les claquettes et le modern jazz.
-			</td>
-		</tr>
-	</table>
+<div class="contenu" style="display: flex; flex-wrap: wrap; flex-direction: row; justify-content: center;">
+
+	<SPAN>
+		<div class="circle" style="background-image:url(images/chorégraphe.png);"></div>
+		<div style="width:200px"> Marianne est la fondatrice de l'école elle enseigne la danse classique et le contemporain. </div>
+	</SPAN>
+	<SPAN>
+		<div class="circle" style="background-image:url(images/Equipelogo.png);"></div>
+		<div style="width:200px"> Jeanne enseigne depuis 10 ans maintenant le hip-hop. </div>
+	</SPAN>
+	<SPAN>
+		<div class="circle" style="background-image:url(images/Equipelogo.png);"></div>
+		<div style="width:200px"> Charlotte a fini sa formation à l'opéra de Paris la saison dernière elle enseigne les claquettes et le modern jazz. </div>
+	</SPAN>
+
 </div>
 
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+</section>
 <?php include('include/footer.php'); ?>
 </body>
 </html>
